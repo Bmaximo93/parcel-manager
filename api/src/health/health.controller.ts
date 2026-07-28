ceb1fcd4fc52d56d333d9a8d19ae9f13d66d1704
Bruno Máximo <Bmaximo93@gmail.com>
2026-07-28 17:58:28 -0300

    feat: check database in health endpoint

--- api/src/health/health.controller.ts
+++ api/src/health/health.controller.ts
@@ -1,9 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly prismaIndicator: PrismaHealthIndicator,
+    private readonly prisma: PrismaService,
+  ) {}
+
   @Get()
-  getHealth() {
-    return { status: 'ok' };
+  @HealthCheck()
+  async getHealth() {
+    return this.health.check([
+      () => this.prismaIndicator.pingCheck('database', this.prisma),
+    ]);
   }
 }
